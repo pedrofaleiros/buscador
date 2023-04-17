bd1b35a57a6d2d946f63adc8d9f05028f2907a8d
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2023-04-17 09:27:47 -0300

    export csv

--- android/app/src/main/kotlin/com/example/buscador/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/buscador/MainActivity.kt
@@ -1,6 +1,24 @@
 package com.example.buscador
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.MethodChannel
+import android.os.Environment
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.buscador/downloads_path"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getDownloadsPath") {
+                val downloadsPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
+                result.success(downloadsPath)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
--- lib/src/features/favorites/view/pages/favorite_page.dart
+++ lib/src/features/favorites/view/pages/favorite_page.dart
@@ -1,14 +1,75 @@
+import 'dart:io';
+
 import 'package:buscador/src/features/favorites/controller/favorite_controller.dart';
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../common/components/bottom_navigation_bar.dart';
 import '../components/favorites_body.dart';
 
+import 'dart:async';
+
 class FavoritePage extends StatelessWidget {
   const FavoritePage({super.key});
 
   static const routeName = '/favorites';
+
+  Future<File> createCSV(List<Map<String, dynamic>> data) async {
+    List<List<dynamic>> rows = [];
+
+    rows.add(['id', 'title', 'author', 'year']);
+
+    data.forEach(
+      (element) {
+        String name = element['author'];
+
+        rows.add(
+          [element['id'], element['title'], element['author'], element['year']],
+        );
+      },
+    );
+
+    String csvData =
+        const ListToCsvConverter().convert(rows, fieldDelimiter: ';');
+
+    print(csvData);
+
+    // final dir = await getExternalStorageDirectory();
+    final downloadsPath = await getDownloadsDirectoryPath();
+
+    if (downloadsPath != null) {
+      final file = File('$downloadsPath/arquivos.csv');
+
+      return file.writeAsString(csvData);
+    } else {
+      throw Exception('Falhou aqui');
+    }
+  }
+
+  Future<String?> getDownloadsDirectoryPath() async {
+    const platform = MethodChannel('com.example.buscador/downloads_path');
+
+    try {
+      final String res = await platform.invokeMethod('getDownloadsPath');
+      return res;
+    } on PlatformException catch (e) {
+      print(e.message);
+      return null;
+    }
+  }
+
+  void _showSnackBar(BuildContext context, String text) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(text),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +85,25 @@ class FavoritePage extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {
+            onPressed: () async {
               final arquivos =
                   context.read<FavoriteController>().getMapArquivos();
-              arquivos.forEach((element) => print(element));
+
+              // PermissionStatus status = await Permission.storage.request();
+              Permission.storage.request().then((status) async {
+                if (status.isGranted) {
+                  createCSV(arquivos).then((value) {
+                    _showSnackBar(
+                      context,
+                      'Arquivo CSV foi salvo na pasta de downloads',
+                    );
+                  }).catchError((e) {
+                    _showSnackBar(context, 'Erro');
+                  });
+                } else {
+                  _showSnackBar(context, 'Permissao Negada');
+                }
+              });
             },
             icon: const Icon(Icons.download),
           )
